b8132954f74dcf5c1ff5e36a7c7daaa4dc2cc65f
Wekoslav Stefanovski <swekster@gmail.com>
2021-02-06 18:44:51 +0100

    Fixed no poster movies

--- display.js
+++ display.js
@@ -2,7 +2,8 @@ var getCastElements = function (movie) { return movie.characters.map(function (c
     var image = character.image
         ? "<img src=\"http://image.tmdb.org/t/p/w185/" + character.image + "\" class=\"image\" />"
         : "<img src=\"https://via.placeholder.com/185x277.png?text=No+Image\" class=\"image\" />";
-    return "<div class=\"character\">\n        " + image + "\n        <div class=\"actor\">" + character.actor + " <br /> as " + character.name + "</div>\n    </div>";
+    var charName = character.name ? " as " + character.name : "";
+    return "<div class=\"character\">\n        " + image + "\n        <div class=\"actor\">" + character.actor + "<br />" + charName + "</div>\n    </div>";
 }).join(""); };
 var getRuntime = function (_a) {
     var runtime = _a.runtime;
@@ -21,5 +22,8 @@ var showResults = function (movie) {
     var backdropUrl = "http://image.tmdb.org/t/p/w1280/" + movie.backdropUrl;
     document.body.style.backgroundImage = "url('" + backdropUrl + "')";
     var result = document.getElementById("result");
-    result.innerHTML = "<div class=\"main-part\">\n        <div class=\"title\">" + movie.title + " (" + movie.releaseDate.getFullYear() + ")</div>\n        <img src=\"http://image.tmdb.org/t/p/w500/" + movie.posterUrl + "\" class=\"poster\" />\n        <div class=\"tagline\">" + movie.tagline + "</div>\n    </div>\n    <div class=\"details-part\">\n        <div class=\"overview\"><strong>Plot summary: </strong>" + movie.overview + "</div>\n        <div class=\"director\"><strong>Directed By: </strong>" + movie.directedBy + "</div>\n        <div class=\"screenplay\"><strong>Written By: </strong>" + movie.writenBy + "</div>\n        " + getRuntime(movie) + "\n        <div class=\"cast\">\n            <div><strong>Starring:</strong></div>\n            " + getCastElements(movie) + "\n        </div>\n    </div>";
+    var poster = movie.posterUrl
+        ? "<img src=\"http://image.tmdb.org/t/p/w500/" + movie.posterUrl + "\" class=\"poster\" />"
+        : "<img src=\"https://via.placeholder.com/500x750.png?text=No+Poster+Available\" class=\"poster\" />";
+    result.innerHTML = "<div class=\"main-part\">\n        <div class=\"title\">" + movie.title + " (" + movie.releaseDate.getFullYear() + ")</div>\n        " + poster + "\n        <div class=\"tagline\">" + movie.tagline + "</div>\n    </div>\n    <div class=\"details-part\">\n        <div class=\"overview\"><strong>Plot summary: </strong>" + movie.overview + "</div>\n        <div class=\"director\"><strong>Directed By: </strong>" + movie.directedBy + "</div>\n        <div class=\"screenplay\"><strong>Written By: </strong>" + movie.writenBy + "</div>\n        " + getRuntime(movie) + "\n        <div class=\"cast\">\n            <div><strong>Starring:</strong></div>\n            " + getCastElements(movie) + "\n        </div>\n    </div>";
 };
--- display.ts
+++ display.ts
@@ -1,11 +1,13 @@
 const getCastElements = (movie: Movie) => movie.characters.map(character => {
-      const image = character.image
+    const image = character.image
         ? `<img src="http://image.tmdb.org/t/p/w185/${character.image}" class="image" />`
         : `<img src="https://via.placeholder.com/185x277.png?text=No+Image" class="image" />`;
 
-      return `<div class="character">
+    const charName = character.name ? ` as ${character.name}` : "";
+
+    return `<div class="character">
         ${image}
-        <div class="actor">${character.actor} <br /> as ${character.name}</div>
+        <div class="actor">${character.actor}<br />${charName}</div>
     </div>`;
 }).join("");
 
@@ -28,9 +30,14 @@ const showResults = (movie: Movie) => {
   document.body.style.backgroundImage = `url('${backdropUrl}')`;
 
   const result = document.getElementById("result");
+
+  const poster = movie.posterUrl 
+    ? `<img src="http://image.tmdb.org/t/p/w500/${movie.posterUrl}" class="poster" />`
+    : `<img src="https://via.placeholder.com/500x750.png?text=No+Poster+Available" class="poster" />`
+
   result.innerHTML = `<div class="main-part">
         <div class="title">${movie.title} (${movie.releaseDate.getFullYear()})</div>
-        <img src="http://image.tmdb.org/t/p/w500/${movie.posterUrl}" class="poster" />
+        ${poster}
         <div class="tagline">${movie.tagline}</div>
     </div>
     <div class="details-part">
